b6ba2dbfbcadb340702ed4db1395771531dcafed
caigenkan <cai_genkan@outlook.com>
2021-03-16 13:45:08 +0900

    从win32的角度应用全屏

--- testdxapi/Main.cpp
+++ testdxapi/Main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <Windows.h>
 
+//#define CREATE_FULLSCREEN_ATINIT
+
 HINSTANCE g_hInst = nullptr;
 HWND g_hWnd = nullptr;
+bool g_isFull = false;
 
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -57,14 +60,38 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
         return E_FAIL;
     }
 
-    // Create window
     g_hInst = hInstance;
-    RECT rc = { 0, 0, 800, 600 };
-    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-    g_hWnd = CreateWindow("WindowTest", "This is a test about using window",
-        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+
+    UINT width = GetSystemMetrics(SM_CXSCREEN);
+    UINT height = GetSystemMetrics(SM_CYSCREEN);
+
+#ifdef CREATE_FULLSCREEN_ATINIT
+    RECT rc = {
+        0, 0,
+        width, height
+    };
+    AdjustWindowRect(&rc, WS_POPUP, FALSE);
+    g_hWnd = CreateWindow("WindowTest",
+        "This is a test about using window",
+        WS_POPUP,
+        0, 0,
+        rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, hInstance,
         nullptr);
+#else
+    RECT rc = {
+        0, 0,
+        1280, 720
+    };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    g_hWnd = CreateWindow("WindowTest",
+        "This is a test about using window",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, hInstance,
+        nullptr);
+#endif // CREATE_FULLSCREEN_ATINIT
     if (!g_hWnd)
     {
         return E_FAIL;
@@ -89,6 +116,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     case WM_DESTROY:
         PostQuitMessage(0);
+        break;
+
+    case WM_KEYDOWN:
+        if (wParam == VK_SPACE)
+        {
+            if (g_isFull)
+            {
+                HWND hDesk;
+                RECT rc;
+                hDesk = GetDesktopWindow();
+                GetWindowRect(hDesk, &rc);
+                int offsetX = rc.right / 2;
+                int offsetY = rc.bottom / 2;
+                int width = 1280;
+                int height = 720;
+                SetWindowLong(hWnd, GWL_STYLE,
+                    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+                SetWindowPos(hWnd, HWND_NOTOPMOST, offsetX - width / 2, offsetY - height / 2,
+                    width, height, SWP_SHOWWINDOW);
+            }
+            else
+            {
+                HWND hDesk;
+                RECT rc;
+                hDesk = GetDesktopWindow();
+                GetWindowRect(hDesk, &rc);
+                SetWindowLong(hWnd, GWL_STYLE, WS_POPUP);
+                SetWindowPos(hWnd, HWND_TOPMOST, 0, 0,
+                    rc.right, rc.bottom, SWP_SHOWWINDOW);
+            }
+
+            g_isFull = !g_isFull;
+        }
         break;
 
     default:
