d25eee2706cf27322cfc5f7c68b1e3edc763552f
caigenkan <cai_genkan@outlook.com>
2021-03-16 15:47:10 +0900

    can refresh one back buffer

--- testdxapi/Main.cpp
+++ testdxapi/Main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <Windows.h>
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+#include <directxcolors.h>
 
 //#define CREATE_FULLSCREEN_ATINIT
 
+// 管理win32方面的内容
 HINSTANCE g_hInst = nullptr;
 HWND g_hWnd = nullptr;
 bool g_isFull = false;
 
+// 管理d3d11方面的内容
+D3D_DRIVER_TYPE g_DriverType;
+D3D_FEATURE_LEVEL g_FeatLevel;
+ID3D11Device* gp_d3dDevice = nullptr;
+ID3D11Device1* gp_d3dDevice1 = nullptr;
+ID3D11DeviceContext* gp_ImmediateContext = nullptr;
+ID3D11DeviceContext1* gp_ImmediateContext1 = nullptr;
+IDXGISwapChain* gp_SwapChain = nullptr;
+IDXGISwapChain1* gp_SwapChain1 = nullptr;
+ID3D11RenderTargetView* gp_RenderTargetView = nullptr;
+
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+HRESULT InitD3D11Device();
+void CleanupDevice();
+void Render();
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(
@@ -19,7 +38,11 @@ int WINAPI WinMain(
 {
     if (FAILED(InitWindow(hInstance, iCmdShow)))
     {
-        return 0;
+        return -1;
+    }
+    if (FAILED(InitD3D11Device()))
+    {
+        return -2;
     }
 
     MSG msg = { 0 };
@@ -32,9 +55,11 @@ int WINAPI WinMain(
         }
         else
         {
-
+            Render();
         }
     }
+
+    CleanupDevice();
 
     return (int)msg.wParam;
 }
@@ -102,6 +127,206 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
     return S_OK;
 }
 
+HRESULT InitD3D11Device()
+{
+    HRESULT hr = S_OK;
+
+    RECT rc;
+    GetClientRect(g_hWnd, &rc);
+    UINT width = rc.right - rc.left;
+    UINT height = rc.bottom - rc.top;
+    UINT deviceCreateFlag = 0;
+#ifdef _DEBUG
+    deviceCreateFlag |= D3D11_CREATE_DEVICE_DEBUG;
+#endif // _DEBUG
+
+    D3D_DRIVER_TYPE driverTypes[] =
+    {
+        D3D_DRIVER_TYPE_HARDWARE,
+        D3D_DRIVER_TYPE_REFERENCE,
+        D3D_DRIVER_TYPE_WARP
+    };
+    UINT numDriverTypes = ARRAYSIZE(driverTypes);
+    D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_11_1,
+        D3D_FEATURE_LEVEL_11_0
+    };
+    UINT numFeatLevels = ARRAYSIZE(featureLevels);
+    g_FeatLevel = featureLevels[0];
+
+    for (UINT i = 0; i < numDriverTypes; i++)
+    {
+        g_DriverType = driverTypes[i];
+        hr = D3D11CreateDevice(nullptr, g_DriverType,
+            nullptr, deviceCreateFlag, featureLevels,
+            numFeatLevels, D3D11_SDK_VERSION,
+            &gp_d3dDevice, &g_FeatLevel, &gp_ImmediateContext);
+
+        if (hr == E_INVALIDARG)
+        {
+            hr = D3D11CreateDevice(nullptr, g_DriverType,
+                nullptr, deviceCreateFlag,
+                featureLevels + 1, numFeatLevels - 1,
+                D3D11_SDK_VERSION, &gp_d3dDevice,
+                &g_FeatLevel, &gp_ImmediateContext);
+        }
+
+        if (SUCCEEDED(hr))
+        {
+            break;
+        }
+    }
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    IDXGIFactory1* dxgiFactory1 = nullptr;
+    {
+        IDXGIDevice* dxgiDevice = nullptr;
+        hr = gp_d3dDevice->QueryInterface(IID_PPV_ARGS(&dxgiDevice));
+        if (SUCCEEDED(hr))
+        {
+            IDXGIAdapter* adapter = nullptr;
+            hr = dxgiDevice->GetAdapter(&adapter);
+            if (SUCCEEDED(hr))
+            {
+                hr = adapter->GetParent(IID_PPV_ARGS(&dxgiFactory1));
+                adapter->Release();
+            }
+            dxgiDevice->Release();
+        }
+    }
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    IDXGIFactory2* dxgiFactory2 = nullptr;
+    hr = dxgiFactory1->QueryInterface(IID_PPV_ARGS(&dxgiFactory2));
+    if (dxgiFactory2)
+    {
+        // 11.1+
+        hr = gp_d3dDevice->QueryInterface(IID_PPV_ARGS(&gp_d3dDevice1));
+        if (SUCCEEDED(hr))
+        {
+            gp_ImmediateContext->QueryInterface(IID_PPV_ARGS(&gp_ImmediateContext1));
+        }
+        DXGI_SWAP_CHAIN_DESC1 dc = {};
+        dc.Width = width;
+        dc.Height = height;
+        dc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        dc.SampleDesc.Count = 1;
+        dc.SampleDesc.Quality = 0;
+        dc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        dc.BufferCount = 1;
+
+        hr = dxgiFactory2->CreateSwapChainForHwnd(gp_d3dDevice, g_hWnd,
+            &dc, nullptr, nullptr, &gp_SwapChain1);
+        if (SUCCEEDED(hr))
+        {
+            hr = gp_SwapChain1->QueryInterface(IID_PPV_ARGS(&gp_SwapChain));
+        }
+
+        dxgiFactory2->Release();
+    }
+    else
+    {
+        // 11.0
+        DXGI_SWAP_CHAIN_DESC dc = {};
+        dc.BufferCount = 1;
+        dc.BufferDesc.Width = width;
+        dc.BufferDesc.Height = height;
+        dc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        dc.BufferDesc.RefreshRate.Numerator = 60;
+        dc.BufferDesc.RefreshRate.Denominator = 1;
+        dc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        dc.OutputWindow = g_hWnd;
+        dc.SampleDesc.Count = 1;
+        dc.SampleDesc.Quality = 0;
+        dc.Windowed = TRUE;
+
+        hr = dxgiFactory1->CreateSwapChain(gp_d3dDevice, &dc, &gp_SwapChain);
+    }
+
+    dxgiFactory1->Release();
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    ID3D11Texture2D* pBackBuffer = nullptr;
+    hr = gp_SwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    hr = gp_d3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &gp_RenderTargetView);
+    pBackBuffer->Release();
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    gp_ImmediateContext->OMSetRenderTargets(1, &gp_RenderTargetView, nullptr);
+
+    D3D11_VIEWPORT vp;
+    vp.Width = (FLOAT)width;
+    vp.Height = (FLOAT)height;
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    gp_ImmediateContext->RSSetViewports(1, &vp);
+
+    return hr;
+}
+
+void CleanupDevice()
+{
+    if (gp_ImmediateContext)
+    {
+        gp_ImmediateContext->ClearState();
+    }
+    if (gp_RenderTargetView)
+    {
+        gp_RenderTargetView->Release();
+    }
+    if (gp_SwapChain1)
+    {
+        gp_SwapChain1->Release();
+    }
+    if (gp_SwapChain)
+    {
+        gp_SwapChain->Release();
+    }
+    if (gp_ImmediateContext1)
+    {
+        gp_ImmediateContext1->Release();
+    }
+    if (gp_ImmediateContext)
+    {
+        gp_ImmediateContext->Release();
+    }
+    if (gp_d3dDevice1)
+    {
+        gp_d3dDevice1->Release();
+    }
+    if (gp_d3dDevice)
+    {
+        gp_d3dDevice->Release();
+    }
+}
+
+void Render()
+{
+    gp_ImmediateContext->ClearRenderTargetView(gp_RenderTargetView, DirectX::Colors::Azure);
+
+    gp_SwapChain->Present(0, 0);
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     PAINTSTRUCT ps;
@@ -127,10 +352,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 RECT rc;
                 hDesk = GetDesktopWindow();
                 GetWindowRect(hDesk, &rc);
-                int offsetX = rc.right / 2;
-                int offsetY = rc.bottom / 2;
-                int width = 1280;
-                int height = 720;
+                UINT offsetX = rc.right / 2;
+                UINT offsetY = rc.bottom / 2;
+                UINT width = 1280;
+                UINT height = 720;
                 SetWindowLong(hWnd, GWL_STYLE,
                     WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
                 SetWindowPos(hWnd, HWND_NOTOPMOST, offsetX - width / 2, offsetY - height / 2,
