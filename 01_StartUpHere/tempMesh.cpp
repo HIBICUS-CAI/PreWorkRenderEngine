24cbd31aed112c81275323eef546341a4eea3264
caigenkan <cai_genkan@outlook.com>
2021-08-18 22:32:58 +0900

    看起来还行原来是忘记换掉双线性的过滤器了，还得手动设置根据斜率的缩放深度偏移

--- 01_StartUpHere/tempMesh.cpp
+++ 01_StartUpHere/tempMesh.cpp
@@ -159,7 +159,7 @@ HRESULT TEMP::PrepareMeshD3D(ID3D11Device* dev,
     }
 
     ZeroMemory(&sampDesc, sizeof(sampDesc));
-    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
     sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
     sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
     sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
