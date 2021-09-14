ec63f6a7ff712ab605d0dba09b3933317f1f383a
caigenkan <cai_genkan@outlook.com>
2021-09-14 12:41:30 +0900

    修改说明

--- 03_RenderSystem_DX11/RSExtraMacro.h
+++ 03_RenderSystem_DX11/RSExtraMacro.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------
 // File: RSExtraMacro.h
 // Proj: RenderSystem_DX11
-// Info: 提供RenderSystem_DX11内可用的相关宏
+// Info: 提供RenderSystem_DX11内可用的额外宏定义
 // Date: 2021.9.14
 // Mail: cai_genkan@outlook.com
 // Comt: NULL
