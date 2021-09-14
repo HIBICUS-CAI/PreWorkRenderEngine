dfa477f76540e19824e6e3a1495fb85993491571
caigenkan <cai_genkan@outlook.com>
2021-09-14 12:32:29 +0900

    准备进行Common内容提取

---
+++ 03_RenderSystem_DX11/RSCommon.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------
+// File: RSCommon.h
+// Proj: RenderSystem_DX11
+// Info: 声明RenderSystem_DX11相关的全局类型
+// Date: 2021.9.14
+// Mail: cai_genkan@outlook.com
+// Comt: 在此lib项目内中提供ExtraMacro
+//---------------------------------------------------------------
+
+#pragma once
+
+// Common here
+
+#ifdef _RS_DX11
+#include "RSExtraMacro.h"
+#endif // _RS_DX11
---
+++ 03_RenderSystem_DX11/RSExtraMacro.h
@@ -0,0 +1,10 @@
+//---------------------------------------------------------------
+// File: RSExtraMacro.h
+// Proj: RenderSystem_DX11
+// Info: 提供RenderSystem_DX11内可用的相关宏
+// Date: 2021.9.14
+// Mail: cai_genkan@outlook.com
+// Comt: NULL
+//---------------------------------------------------------------
+
+#pragma once
