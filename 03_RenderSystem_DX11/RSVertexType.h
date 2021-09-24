1353cbf38ac33f47a3acc1246f247307a7587618
caigenkan <cai_genkan@outlook.com>
2021-09-24 18:26:37 +0900

    补上文件头注释 让自己看上去像是还在学习

--- 03_RenderSystem_DX11/RSVertexType.h
+++ 03_RenderSystem_DX11/RSVertexType.h
@@ -1,3 +1,12 @@
+﻿//---------------------------------------------------------------
+// File: RSVertexType.h
+// Proj: RenderSystem_DX11
+// Info: 声明RenderSystem_DX11相关所使用的顶点类型
+// Date: 2021.9.19
+// Mail: cai_genkan@outlook.com
+// Comt: 目前考虑仅在RenderSystem_DX11中使用
+//---------------------------------------------------------------
+
 #pragma once
 
 #include <DirectXMath.h>
