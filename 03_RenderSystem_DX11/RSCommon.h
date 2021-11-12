ea96d4f234b3d0ab00c086494154c699d227442c
caigenkan <cai_genkan@outlook.com>
2021-11-12 15:23:06 +0900

    确定粒子info构造体内容

--- 03_RenderSystem_DX11/RSCommon.h
+++ 03_RenderSystem_DX11/RSCommon.h
@@ -106,14 +106,51 @@ struct RS_LIGHT_INFO
     float mSpotPower = 0.f;
 };
 
+enum class PARTICAL_TEXTURE
+{
+    WHITE_CIRCLE,
+
+    PARTICAL_TEXTURE_SIZE
+};
+
 struct PARTICAL_EMITTER_INFO
 {
-
+    float mEmitNumPerSecond = 0.f;
+    DirectX::XMFLOAT3 mPosition = {};
+    DirectX::XMFLOAT3 mVelocity = {};
+    DirectX::XMFLOAT3 mPosVariance = {};
+    float mVelVariance = 0.f;
+    DirectX::XMFLOAT3 mAcceleration = {};
+    float mParticalMass = 0.f;
+    float mLifeSpan = 0.f;
+    float mOffsetStartSize = 0.f;
+    float mOffsetEndSize = 0.f;
+    DirectX::XMFLOAT4 mOffsetStartColor = {};
+    DirectX::XMFLOAT4 mOffsetEndColor = {};
+    bool mEnableStreak = false;
+    PARTICAL_TEXTURE mTextureID = PARTICAL_TEXTURE::WHITE_CIRCLE;
 };
 
 struct RS_PARTICAL_EMITTER_INFO
 {
-
+    UINT mEmitterIndex = 0;
+    float mEmitNumPerSecond = 0.f;
+    UINT mNumToEmit = 0;
+    float mAccumulation = 0.f;
+    DirectX::XMFLOAT3 mPosition = {};
+    DirectX::XMFLOAT3 mVelocity = {};
+    DirectX::XMFLOAT3 mPosVariance = {};
+    float mVelVariance = 0.f;
+    DirectX::XMFLOAT3 mAcceleration = {};
+    float mParticalMass = 0.f;
+    float mLifeSpan = 0.f;
+    float mOffsetStartSize = 0.f;
+    float mOffsetEndSize = 0.f;
+    DirectX::XMFLOAT4 mOffsetStartColor = {};
+    DirectX::XMFLOAT4 mOffsetEndColor = {};
+    UINT mTextureID = (UINT)(PARTICAL_TEXTURE::WHITE_CIRCLE);
+    UINT mStreakFlg = 0;
+    UINT mMiscFlg = 0;
 };
 
 struct MATERIAL_INFO
