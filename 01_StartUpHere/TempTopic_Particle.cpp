4084e4f0a2582038bf3b5c22f5a0ba8fa9102620
caigenkan <cai_genkan@outlook.com>
2021-11-16 15:58:41 +0900

    准备了一部分的成员变量，被赶去做苦力了

--- 01_StartUpHere/TempTopic_Particle.cpp
+++ 01_StartUpHere/TempTopic_Particle.cpp
@@ -1,1 +1,60 @@
 #include "TempTopic_Particle.h"
+#include "RSRoot_DX11.h"
+
+RSPass_PriticleSimulate::RSPass_PriticleSimulate(
+    std::string& _name, PASS_TYPE _type, RSRoot_DX11* _root) :
+    RSPass_Base(_name, _type, _root)
+{
+
+}
+
+RSPass_PriticleSimulate::RSPass_PriticleSimulate(
+    const RSPass_PriticleSimulate& _source) :
+    RSPass_Base(_source)
+{
+
+}
+
+RSPass_PriticleSimulate::~RSPass_PriticleSimulate()
+{
+
+}
+
+RSPass_PriticleSimulate* RSPass_PriticleSimulate::ClonePass()
+{
+    return new RSPass_PriticleSimulate(*this);
+}
+
+bool RSPass_PriticleSimulate::InitPass()
+{
+    // TEMP-------------
+    return false;
+}
+
+void RSPass_PriticleSimulate::ReleasePass()
+{
+
+}
+
+void RSPass_PriticleSimulate::ExecuatePass()
+{
+
+}
+
+bool RSPass_PriticleSimulate::CreateShaders()
+{
+    // TEMP-----------
+    return false;
+}
+
+bool RSPass_PriticleSimulate::CreateBuffers()
+{
+    // TEMP---------------
+    return false;
+}
+
+bool RSPass_PriticleSimulate::CreateViews()
+{
+    // TEMP-----------------
+    return false;
+}
--- 01_StartUpHere/TempTopic_Particle.h
+++ 01_StartUpHere/TempTopic_Particle.h
@@ -1,1 +1,64 @@
 #pragma once
+
+#include "RSPass_Base.h"
+#include <string>
+
+class RSPass_PriticleSimulate :public RSPass_Base
+{
+public:
+    RSPass_PriticleSimulate(std::string& _name, PASS_TYPE _type,
+        class RSRoot_DX11* _root);
+    RSPass_PriticleSimulate(const RSPass_PriticleSimulate& _source);
+    virtual ~RSPass_PriticleSimulate();
+
+public:
+    virtual RSPass_PriticleSimulate* ClonePass() override;
+
+    virtual bool InitPass();
+
+    virtual void ReleasePass();
+
+    virtual void ExecuatePass();
+
+private:
+    bool CreateShaders();
+    bool CreateBuffers();
+    bool CreateViews();
+
+private:
+    ID3D11Buffer* mParticlePartA;
+    ID3D11ShaderResourceView* mPartA_Srv;
+    ID3D11UnorderedAccessView* mPartA_Uav;
+
+    ID3D11Buffer* mParticalPartB;
+    ID3D11UnorderedAccessView* mPartB_Uav;
+
+    ID3D11Buffer* mViewspacePosBuffer;
+    ID3D11ShaderResourceView* mViewSpacePos_Srv;
+    ID3D11UnorderedAccessView* mViewSpacePos_Uav;
+
+    ID3D11Buffer* mMaxRadiusBuffer;
+    ID3D11ShaderResourceView* mMaxRadius_Srv;
+    ID3D11UnorderedAccessView* mMaxRadius_Uav;
+
+    ID3D11Buffer* mStridedCoarseCullBuffer;
+    ID3D11ShaderResourceView* mStridedCoarseCull_Srv;
+    ID3D11UnorderedAccessView* mStridedCoarseCull_Uav;
+
+    ID3D11Buffer* mStridedCoarseCullCounterBuffer;
+    ID3D11ShaderResourceView* mStridedCoarseCullCounter_Srv;
+    ID3D11UnorderedAccessView* mStridedCoarseCullCounter_Uav;
+
+    ID3D11Buffer* mDeadListBuffer;
+    ID3D11UnorderedAccessView* mDeadList_Uav;
+
+    ID3D11Buffer* mAliveIndexBuffer;
+    ID3D11ShaderResourceView* mAliveIndex_Srv;
+    ID3D11UnorderedAccessView* mAliveIndex_Uav;
+
+    ID3D11Buffer* mDeadListConstantBuffer;
+    ID3D11Buffer* mActiveListConstantBuffer;
+
+    ID3D11Buffer* mEmitterConstantBuffer;
+    ID3D11Buffer* mTilingConstantBuffer;
+};
